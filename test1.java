b9cbc980097a02b4bcf9c4e7dd09715fac94862a
Bhaskardigumrthy1405@gmail.com <bhasihyesung73@gmail.com>
2021-12-03 08:53:39 +0530

    second commit

---
+++ test1.java
@@ -0,0 +1,20 @@
+public static void main()
+{
+hello werlod 
+}
+
+
+
+
+
+
+
+git remote add origin https://github.com/bhasi73/xxx.git
+git branch -M main
+git push -u origin main
+*******************************************
+
+cd existing_repo
+git remote add origin https://gitlab.com/bhaskardigumrthy1405/dex.git
+git branch -M main
+git push -uf origin main
